da586dc0d91cf56cf7526ecbbcbe69ab1448df8f
s-tanaka <seito.tanaka.dev@gmail.com>
2021-07-23 02:53:50 +0900

    get range

--- test/git.lisp
+++ test/git.lisp
@@ -8,9 +8,17 @@
 
 (in-suite git)
 
+(defparameter src-path "test/fixtures/react-typescript-todo")
+
 (test get-diff
   (is (equal '(("start" . 13) ("end" . 15)
                ("start" . 23) ("end" . 23))
-             (inga:get-diff "a690a51"))))
+             (inga:get-diff "a690a51" "69bf5cb"))))
+
+(test diffが1行のみ
+  (is (equal '(("start" . 6) ("end" . 6))
+             (inga:get-diff "a690a51" "dc33553"))))
  
 (run! 'git)
+;;(run! 'get-diff)
+;;(run! 'diffが1行のみ)
