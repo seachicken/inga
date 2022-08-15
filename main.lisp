655a8680dd68e1e764b7c4d6d46c0a73c0d98052
seito.tanaka <seito.tanaka.dev@gmail.com>
2022-08-15 21:57:40 +0900

    Delete lombok dependencies

--- main.lisp
+++ main.lisp
@@ -114,8 +114,8 @@
     (setf *jdtls-id* 0)
     (setq *jdtls* 
           (uiop:launch-program
-            (format nil "~a/libs/jdtls/bin/jdtls --jvm-arg=-javaagent:~a/libs/lombok.jar -data ~a/libs/jdtls/workspace"
-                    (uiop:getenv "INGA_HOME") (uiop:getenv "INGA_HOME") (uiop:getenv "INGA_HOME"))
+            (format nil "~a/libs/jdtls/bin/jdtls -data ~a/libs/jdtls/workspace"
+                    (uiop:getenv "INGA_HOME") (uiop:getenv "INGA_HOME"))
             :input :stream :output :stream))
     (exec-jdtls-initialize back-path)))
 
