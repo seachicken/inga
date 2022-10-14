5a618a423139429896ceb2dec17c21d083882e07
seito.tanaka <seito.tanaka.dev@gmail.com>
2022-10-14 23:10:34 +0900

    Fix getenv call

--- logger.lisp
+++ logger.lisp
@@ -3,10 +3,7 @@
    (:export #:log-debug))
 (in-package #:inga/logger)
 
-(defparameter *debug* (when (uiop:getenv "INGA_DEBUG") t))
-
 (defun log-debug (content)
-  (format t "env debug: ~a, debug: ~a~%" (uiop:getenv "INGA_DEBUG") *debug*)
-  (when *debug*
+  (when (uiop:getenv "INGA_DEBUG")
     (format t "~&~a~%" content)))
 
