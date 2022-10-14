f4d0b232fd339633ae12f7c8d926bd7b885b2185
seito.tanaka <seito.tanaka.dev@gmail.com>
2022-10-14 22:57:37 +0900

    Add debug log

--- logger.lisp
+++ logger.lisp
@@ -6,6 +6,7 @@
 (defparameter *debug* (when (uiop:getenv "INGA_DEBUG") t))
 
 (defun log-debug (content)
+  (format t "env debug: ~a, debug: ~a~%" (uiop:getenv "INGA_DEBUG") *debug*)
   (when *debug*
     (format t "~&~a~%" content)))
 
