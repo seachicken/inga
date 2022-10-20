a9bc4c693b567c0146cedcb43daae5dcb6cea93e
seito.tanaka <seito.tanaka.dev@gmail.com>
2022-10-20 22:24:02 +0900

    Remove initialize sleep

--- language-client/java.lisp
+++ language-client/java.lisp
@@ -16,9 +16,7 @@
           (uiop:launch-program
             (format nil "~a/libs/jdtls/bin/jdtls -data ~a/libs/jdtls/workspace --jvm-arg=-javaagent:~a/libs/lombok.jar" home home home)
             :input :stream :output :stream)))
-  (initialize-client client)
-  ;; TODO: wait until "textDocument/references" is fully working
-  (sleep (* 60 5)))
+  (initialize-client client))
 
 (defmethod stop-client ((client language-client-java))
   (uiop:close-streams (client-process client)))
