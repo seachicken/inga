ea5b75a325940330992c5ad48bccc89b22f690c1
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-05-02 12:07:05 +0900

    Remove jdtls dependencies

--- language-client/java.lisp
+++ language-client/java.lisp
@@ -13,44 +13,11 @@
 (defmethod make-client ((kind (eql :java)) path cache)
   (make-instance 'language-client-java :path path :cache cache))
 
-(defmethod start-client ((client language-client-java))
-  (let ((home (uiop:getenv "INGA_HOME")))
-    (setf (client-process client)
-          (uiop:launch-program
-            ;; https://github.com/redhat-developer/vscode-java/blob/f93cc8bb574956aa27edaff32a798f33283c9195/package.json#L218
-            (format nil "~a/libs/jdtls/bin/jdtls -data ~a/libs/jdtls/workspace --jvm-arg=-XX:+UseParallelGC --jvm-arg=-XX:GCTimeRatio=4 --jvm-arg=-XX:AdaptiveSizePolicyWeight=90 --jvm-arg=-Dsun.zip.disableMemoryMapping=true --jvm-arg=-Xmx1g --jvm-arg=-Xms100m --jvm-arg=-javaagent:~a/libs/lombok.jar --jvm-arg=-Djava.import.generatesMetadataFilesAtProjectRoot=false" home home home)
-            :input :stream :output :stream)))
-  (initialize-client client))
+(defmethod start-client ((client language-client-java)))
 
-(defmethod stop-client ((client language-client-java))
-  (uiop:close-streams (client-process client)))
+(defmethod stop-client ((client language-client-java)))
 
-(defmethod references-client ((client language-client-java) pos)
-  (let ((cache (get-value (client-cache client) (get-references-key pos)))
-        (full-path (namestring
-                     (uiop:merge-pathnames* (cdr (assoc :path pos))
-                                            (client-path client)))))
-    (values
-      (if cache
-          cache
-          (progn
-            (increment-req-id client)
-            (let ((refs (exec-command client (format nil "{\"jsonrpc\":\"2.0\",\"id\":~a,\"method\":\"textDocument/references\",\"params\":{\"textDocument\":{\"uri\":\"file://~a\"},\"position\":{\"line\":~a,\"character\":~a},\"context\":{\"includeDeclaration\":false}}}"
-                                                     (client-req-id client) full-path
-                                                     ;; the zero-based line and offset
-                                                     (- (cdr (assoc :line pos)) 1) (- (cdr (assoc :offset pos)) 1))))
-                  result)
-              (setf result (mapcar
-                             (lambda (ref)
-                               (list
-                                 (cons :path (enough-namestring (subseq (jsown:val ref "uri") 7)
-                                                                (client-path client)))
-                                 (cons :line (+ (jsown:val (jsown:val (jsown:val ref "range") "start") "line") 1))
-                                 (cons :offset (+ (jsown:val (jsown:val (jsown:val ref "range") "start") "character") 1))))
-                             (jsown:val refs "result")))
-              (put-value (client-cache client) (get-references-key pos) result)
-              result)))
-      (when cache t))))
+(defmethod references-client ((client language-client-java) pos))
 
 (defmethod get-command ((client language-client-java) command)
   (format nil "Content-Length: ~a~c~c~c~c~a"
