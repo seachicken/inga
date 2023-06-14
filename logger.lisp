605a66a8122edbeb5d52f17acf7be0144e29e7bd
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-06-14 22:49:04 +0900

    fix

--- logger.lisp
+++ logger.lisp
@@ -6,6 +6,6 @@
 
 (defun log-debug (content)
   (let ((debug (uiop:getenv "INGA_DEBUG")))
-    (when (not (equal debug "0"))
+    (when (equal debug "1")
       (format t "~&~a ~a~%" (local-time:now) content))))
 
