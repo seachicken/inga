8ad9f3de67116c440634672a7f507f98fae781e4
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-09-19 11:06:42 +0900

    Fix tests

--- test/main.lisp
+++ test/main.lisp
@@ -22,20 +22,22 @@
           '(((:path . "src/App/NewTodoInput/index.tsx")
              (:name . "input")
              (:line . 34) (:offset . 10)))
-          (mapcar (lambda (e) (cdr (assoc :entrypoint e)))
-                  (inga/main::analyze-by-range
-                    ctx
-                    `((:path . "src/functions.ts")
-                      ,(cons :start-offset
-                             (inga/ast-analyzer:convert-to-top-offset
-                               *front-path*
-                               "src/functions.ts"
-                               '((:line . 2) (:offset . 0))))
-                      ,(cons :end-offset
-                             (inga/ast-analyzer:convert-to-top-offset
-                               *front-path*
-                               "src/functions.ts"
-                               '((:line . 2) (:offset . -1)))))))))
+          (remove-duplicates
+            (mapcar (lambda (e) (cdr (assoc :entrypoint e)))
+                    (inga/main::analyze-by-range
+                      ctx
+                      `((:path . "src/functions.ts")
+                        ,(cons :start-offset
+                               (inga/ast-analyzer:convert-to-top-offset
+                                 *front-path*
+                                 "src/functions.ts"
+                                 '((:line . 2) (:offset . 0))))
+                        ,(cons :end-offset
+                               (inga/ast-analyzer:convert-to-top-offset
+                                 *front-path*
+                                 "src/functions.ts"
+                                 '((:line . 2) (:offset . -1)))))))
+            :test #'equal)))
     (inga/main::stop ctx)))
 
 (def-suite java)
@@ -52,20 +54,22 @@
           '(((:path . "src/main/java/io/spring/api/ArticlesApi.java")
              (:name . "getArticles")
              (:line . 49) (:offset . 25)))
-          (mapcar (lambda (e) (cdr (assoc :entrypoint e)))
-                  (inga/main::analyze-by-range
-                    ctx
-                    `((:path . "src/main/java/io/spring/application/ArticleQueryService.java")
-                      ,(cons :start-offset
-                             (inga/ast-analyzer:convert-to-top-offset
-                               *back-path*
-                               "src/main/java/io/spring/application/ArticleQueryService.java"
-                               '((:line . 105) (:offset . 0))))
-                      ,(cons :end-offset
-                             (inga/ast-analyzer:convert-to-top-offset
-                               *back-path*
-                               "src/main/java/io/spring/application/ArticleQueryService.java"
-                               '((:line . 105) (:offset . -1)))))))))
+          (remove-duplicates
+            (mapcar (lambda (e) (cdr (assoc :entrypoint e)))
+                    (inga/main::analyze-by-range
+                      ctx
+                      `((:path . "src/main/java/io/spring/application/ArticleQueryService.java")
+                        ,(cons :start-offset
+                               (inga/ast-analyzer:convert-to-top-offset
+                                 *back-path*
+                                 "src/main/java/io/spring/application/ArticleQueryService.java"
+                                 '((:line . 105) (:offset . 0))))
+                        ,(cons :end-offset
+                               (inga/ast-analyzer:convert-to-top-offset
+                                 *back-path*
+                                 "src/main/java/io/spring/application/ArticleQueryService.java"
+                                 '((:line . 105) (:offset . -1)))))))
+            :test #'equal)))
     (inga/main::stop ctx)))
 
 (test analyze-by-range-for-micro-services
@@ -79,30 +83,23 @@
              (:line . 18) (:offset . 17)) 
             ((:path . "api-service/src/main/java/com/baeldung/apiservice/adapters/http/TasksController.java")
              (:name . "getTaskById")
-             (:line . 25) (:offset . 25))
-            ((:path . "api-service/src/main/java/com/baeldung/apiservice/adapters/http/TasksController.java")
-             (:name . "getTaskById")
-             (:line . 25) (:offset . 25))
-            ((:path . "api-service/src/main/java/com/baeldung/apiservice/adapters/http/TasksController.java")
-             (:name . "getTaskById")
-             (:line . 25) (:offset . 25))
-            ((:path . "api-service/src/main/java/com/baeldung/apiservice/adapters/http/TasksController.java")
-             (:name . "getTaskById")
              (:line . 25) (:offset . 25)))
-          (mapcar (lambda (e) (cdr (assoc :entrypoint e)))
-                  (inga/main::analyze-by-range
-                    ctx
-                    `((:path . "users-service/src/main/java/com/baeldung/usersservice/adapters/http/UsersController.java")
-                      ,(cons :start-offset
-                             (inga/ast-analyzer:convert-to-top-offset
-                               *lightrun-path*
-                               "users-service/src/main/java/com/baeldung/usersservice/adapters/http/UsersController.java"
-                               '((:line . 39) (:offset . 0))))
-                      ,(cons :end-offset
-                             (inga/ast-analyzer:convert-to-top-offset
-                               *lightrun-path*
-                               "users-service/src/main/java/com/baeldung/usersservice/adapters/http/UsersController.java"
-                               '((:line . 39) (:offset . -1)))))))))
+          (remove-duplicates
+            (mapcar (lambda (e) (cdr (assoc :entrypoint e)))
+                    (inga/main::analyze-by-range
+                      ctx
+                      `((:path . "users-service/src/main/java/com/baeldung/usersservice/adapters/http/UsersController.java")
+                        ,(cons :start-offset
+                               (inga/ast-analyzer:convert-to-top-offset
+                                 *lightrun-path*
+                                 "users-service/src/main/java/com/baeldung/usersservice/adapters/http/UsersController.java"
+                                 '((:line . 39) (:offset . 0))))
+                        ,(cons :end-offset
+                               (inga/ast-analyzer:convert-to-top-offset
+                                 *lightrun-path*
+                                 "users-service/src/main/java/com/baeldung/usersservice/adapters/http/UsersController.java"
+                                 '((:line . 39) (:offset . -1)))))))
+            :test #'equal)))
     (inga/main::stop ctx)))
 
 (test analyze-by-range-for-constraint-validator
