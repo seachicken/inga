5518ec39ef96ccd88561cbe260e1b6c4080d876c
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-10-28 21:54:52 +0900

    Do not cache in development

--- cache.lisp
+++ cache.lisp
@@ -8,12 +8,14 @@
     `(progn
        (defparameter ,cache-sym (make-hash-table :test 'equal))
        (defun ,name ,params
-         (let* ((args (list ,@params))
-                (cache-key (intern (format nil "~a" (list ,@params))))
-                (cached-result (gethash cache-key ,cache-sym)))
-           (if cached-result
-               (if (eq cached-result 'empty) nil cached-result)
-               (let ((result (progn ,@body)))
-                 (setf (gethash cache-key ,cache-sym) (or result 'empty))
-                 result)))))))
+         (if (equal (uiop:getenv "INGA_DEBUG") "1")
+             (progn ,@body)
+             (let* ((args (list ,@params))
+                    (cache-key (intern (format nil "~a" (list ,@params))))
+                    (cached-result (gethash cache-key ,cache-sym)))
+               (if cached-result
+                   (if (eq cached-result 'empty) nil cached-result)
+                   (let ((result (progn ,@body)))
+                     (setf (gethash cache-key ,cache-sym) (or result 'empty))
+                     result))))))))
 
