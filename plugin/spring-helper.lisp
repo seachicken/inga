9da6bfd76c691dd63776f6e5d08544a60d42d7bd
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-12-09 13:58:28 +0900

    Refactor

--- plugin/spring-helper.lisp
+++ plugin/spring-helper.lisp
@@ -1,10 +1,12 @@
 (defpackage #:inga/plugin/spring-helper
   (:use #:cl)
+  (:import-from #:alexandria
+                #:switch)
   (:export #:convert-to-http-method))
 (in-package #:inga/plugin/spring-helper)
 
 (defun convert-to-http-method (type)
-  (alexandria:switch (type :test #'equal)
+  (switch (type :test #'equal)
     ("GetMapping" "GET")
     ("PostMapping" "POST")  
     ("PutMapping" "PUT")
