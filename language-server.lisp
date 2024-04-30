8a773e68fbf42135c05f9c163ff3be3fab61e20b
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-05-01 08:53:43 +0900

    test

--- language-server.lisp
+++ language-server.lisp
@@ -2,21 +2,40 @@
   (:use #:cl)
   (:import-from #:flexi-streams)
   (:import-from #:jsown)
+  (:import-from #:inga/git
+                #:get-diff)
+  (:import-from #:inga/main
+                #:analyze
+                #:to-json)
   (:export #:run))
 (in-package #:inga/language-server)
 
-(defun run ()
-  (let ((msg (loop while *standard-input* do
-                   (let* ((json (extract-json *standard-input*))
-                          (result (when json (jsown:parse json))))
-                     (return result)))))
-    (when msg
-      (format t "Content-Length: 53~C~%~C~%~a~%" #\return #\return
-              "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":{\"capabilities\":{}}}")
-      (force-output t)
-      (format t "received message: ~a, msg: ~a~%" (jsown:val msg "method") msg)
-      )
-    (run)))
+(defun run (params)
+  (let* ((msg (loop while *standard-input* do
+                    (let* ((json (extract-json *standard-input*))
+                           (result (when json (jsown:parse json))))
+                      (return result))))
+         (method (when msg (jsown:val msg "method"))))
+    (when method
+      (cond
+        ((equal method "initialize")
+         (format t "Content-Length: 53~C~%~C~%~a~%" #\return #\return
+                 "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":{\"capabilities\":{}}}")
+         (force-output t))
+        ((equal method "initialized")
+         (destructuring-bind (&key root-path include exclude base-commit mode) params
+           (let* ((diffs (get-diff root-path base-commit))
+                  (ctx (inga/main::start root-path
+                              '(:java)
+                              :include include :exclude exclude))
+                  (results (inga/main:to-json (inga/main:analyze ctx diffs) root-path)))
+             ;; TODO: count length
+             (format t "Content-Length: 100~C~%~C~%~%" #\return #\return)
+             (format t "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":~a}" results)
+             (force-output t))))
+        ((equal method "textDocument/didChange")
+         )))
+    (run params)))
 
 (defun extract-json (stream)
   ;; Content-Length: 99
