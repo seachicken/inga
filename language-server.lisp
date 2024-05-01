473be32524910d2cb90e6f481c5d07cb412c828d
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-05-01 20:59:23 +0900

    Update html report when message received

--- language-server.lisp
+++ language-server.lisp
@@ -2,26 +2,52 @@
   (:use #:cl)
   (:import-from #:flexi-streams)
   (:import-from #:jsown)
+  (:import-from #:inga/git
+                #:get-diff)
+  (:import-from #:inga/main
+                #:analyze
+                #:to-json)
   (:export #:run))
 (in-package #:inga/language-server)
 
-(defun run ()
-  (let ((msg (loop while *standard-input* do
-                   (let* ((json (extract-json *standard-input*))
-                          (result (when json (jsown:parse json))))
-                     (return result)))))
-    (when msg
-      (format t "Content-Length: 53~C~%~C~%~a~%" #\return #\return
-              "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":{\"capabilities\":{}}}")
-      (force-output t))
-    (run)))
+(defun run (params)
+  (destructuring-bind (&key root-path include exclude base-commit mode) params
+    (let* ((ctx (inga/main::start root-path '(:java) :include include :exclude exclude)))
+      (handle-msg params ctx)
+      (inga/main::stop ctx))))
+
+(defun handle-msg (params ctx)
+  (destructuring-bind (&key root-path include exclude base-commit mode) params
+    (let* ((msg (loop while *standard-input* do
+                      (let* ((json (extract-json *standard-input*))
+                             (result (when json (jsown:parse json))))
+                        (return result))))
+           (method (when msg (jsown:val msg "method"))))
+      (when method
+        (cond
+          ((equal method "initialize")
+           (format t "Content-Length: 53~c~c~c~c~a" #\return #\linefeed
+                   #\return #\linefeed
+                   "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":{\"capabilities\":{}}}")
+           (force-output))
+          ((or (equal method "initialized")
+               (equal method "textDocument/didChange"))
+           (let* ((diffs (get-diff root-path base-commit))
+                  (results (inga/main:to-json (inga/main:analyze ctx diffs) root-path)))
+             (ensure-directories-exist "reports/")
+             (with-open-file (out "reports/report.json"
+                                  :direction :output
+                                  :if-exists :supersede
+                                  :if-does-not-exist :create)
+               (format out "~a" results))
+             (format t "~%~a~%" results)))))
+      (handle-msg params ctx))))
 
 (defun extract-json (stream)
   ;; Content-Length: 99
   (let* ((input (read-line stream nil))
          (len (when (>= (length input) 16) (parse-integer (subseq input 16)))))
     (unless len (return-from extract-json))
-
     ;; newline
     (read-line stream)
     ;; JSON
--- main.lisp
+++ main.lisp
@@ -11,8 +11,6 @@
                 #:start-client
                 #:stop-client
                 #:references-client)
-  (:import-from #:inga/language-server
-                #:run)
   (:import-from #:inga/traversal
                 #:convert-to-pos
                 #:convert-to-top-offset
@@ -33,7 +31,10 @@
                 #:inga-error)
   (:import-from #:inga/logger
                 #:log-debug)
-  (:export #:command))
+  (:export #:parse-argv
+           #:command
+           #:analyze
+           #:to-json))
 (in-package #:inga/main)
 
 (defparameter *include-typescript*
@@ -46,27 +47,22 @@
 (define-condition inga-error-option-not-found (inga-error) ())
 (define-condition inga-error-context-not-found (inga-error) ())
 
-(defun command (&rest argv)
+(defun command (params)
   (handler-case
-    (destructuring-bind (&key root-path include exclude base-commit mode) (parse-argv argv)
-      (cond
-        ((equal mode "cli")
-         (let ((diffs (get-diff root-path base-commit)))
-           (let ((ctx (start root-path
-                             (filter-active-context (get-analysis-kinds diffs) (get-env-kinds))
-                             :include include :exclude exclude)))
-             (let ((results (analyze ctx diffs)))
-               (ensure-directories-exist "reports/")
-               (with-open-file (out "reports/report.json"
-                                    :direction :output
-                                    :if-exists :supersede
-                                    :if-does-not-exist :create)
-                 (format out "~a" (to-json results root-path)))
-               (format t "~%~a~%" (to-json results root-path)))
-             (stop ctx))))
-        ((equal mode "server")
-         (inga/language-server:run))
-        (t (error 'unknown-server))))
+    (destructuring-bind (&key root-path include exclude base-commit mode) params
+      (let* ((diffs (get-diff root-path base-commit))
+             (ctx (start root-path
+                         (filter-active-context (get-analysis-kinds diffs) (get-env-kinds))
+                         :include include :exclude exclude))
+             (results (analyze ctx diffs)))
+          (ensure-directories-exist "reports/")
+          (with-open-file (out "reports/report.json"
+                               :direction :output
+                               :if-exists :supersede
+                               :if-does-not-exist :create)
+            (format out "~a" (to-json results root-path)))
+          (format t "~%~a~%" (to-json results root-path))
+        (stop ctx)))
     (inga-error (e) (format t "~a~%" e))))
 
 (defun parse-argv (argv)
@@ -124,11 +120,11 @@
                    :ast-index index
                    :traversals (list
                                     (start-traversal :java
-                                                        (or include *include-java*)
-                                                        exclude root-path index)
+                                                     (or include *include-java*)
+                                                     exclude root-path index)
                                     (start-traversal :kotlin
-                                                        (or include *include-java*)
-                                                        exclude root-path index))
+                                                     (or include *include-java*)
+                                                     exclude root-path index))
                    :processes (list
                                 (inga/plugin/spring/spring-property-loader:start root-path)
                                 (inga/plugin/jvm-dependency-loader:start root-path))))
