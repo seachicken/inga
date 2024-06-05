ee4b12687107a9d40aec101047f5e4ee3db394a4
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-06-05 10:50:46 +0900

    Get local change diff by default

--- git.lisp
+++ git.lisp
@@ -8,7 +8,9 @@
 
 (defun get-diff (project-path base-commit)
   (let ((diff (uiop:run-program
-                (format nil "(cd ~a && git diff ~a --unified=0 --)" project-path base-commit)
+                (format nil "(cd ~a && git diff ~a --unified=0 --)"
+                        project-path
+                        (if base-commit base-commit ""))
                 :output :string)))
     (let ((ranges (make-array 10 :fill-pointer 0 :adjustable t)) to-path)
       (with-input-from-string (in diff)
