b3b3a61eac9ad472ff14309b1525d36bdce75c8b
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-10-19 19:12:36 +0900

    test

--- server.lisp
+++ server.lisp
@@ -176,15 +176,15 @@
                                        (context-include ctx)
                                        (context-exclude ctx))
                (log-debug (format nil "update-index path: ~a" path))
-               (update-index (context-ast-index ctx) path)
-               (process-output-if-present
-                 (analyze
-                   ctx diff
-                   :success (lambda (results)
-                              (process-output-if-present results output-path root-path))
-                   :failure (lambda (failures)
-                              (output-error failures output-path root-path)))
-                 output-path root-path))))))
+               (update-index (context-ast-index ctx) path))
+             (process-output-if-present
+               (analyze
+                 ctx diff
+                 :success (lambda (results)
+                            (process-output-if-present results output-path root-path))
+                 :failure (lambda (failures)
+                            (output-error failures output-path root-path)))
+               output-path root-path)))))
       (process-msg-if-present (dequeue-msg) ctx root-path output-path temp-path base-commit root-host-paths))))
 
 (defun process-output-if-present (output output-path root-path)
