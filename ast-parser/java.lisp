3ee91dab3233fa6bae3bac5b2f7d362965896e5e
seito.tanaka <seito.tanaka.dev@gmail.com>
2025-03-21 06:51:50 +0900

    test

--- ast-parser/java.lisp
+++ ast-parser/java.lisp
@@ -13,20 +13,12 @@
   ())
 
 (defmethod start ((file-type (eql :java)))
-  (inga/logger:log-info (format nil "java? ~a~%" (format nil "~{~a~^ ~}"
-                                                         `(,(if (equal (uiop:getenv "JAVA_HOME") "/usr/local/cri")
-                                                                "/usr/local/cri-parser/bin/java"
-                                                                "java")
-                                                            "-jar" ,(format nil "~a/libs/javaparser.jar" (uiop:getenv "INGA_HOME"))
-                                                            "--add-opens" "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED"
-                                                            "--add-opens" "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED"
-                                                            "--add-opens" "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"))))
   (make-instance
     'ast-parser-java
     :process (uiop:launch-program
                (format nil "~{~a~^ ~}"
                        `(,(if (equal (uiop:getenv "JAVA_HOME") "/usr/local/cri")
-                                     "java"
+                                     "/usr/local/cri-parser/bin/java"
                                      "java")
                           "-jar" ,(format nil "~a/libs/javaparser.jar" (uiop:getenv "INGA_HOME"))
                           "--add-opens" "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED"
